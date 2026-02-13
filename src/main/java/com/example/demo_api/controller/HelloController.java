e8d49875dd869b406ed63441d84bda1b48a7445d
azhararman <azhararman64@gmail.com>
2026-02-13 22:47:40 +0530

    updated CODE

--- src/main/java/com/example/demo_api/controller/HelloController.java
+++ src/main/java/com/example/demo_api/controller/HelloController.java
@@ -10,6 +10,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello from Jenkins + Docker + Git Demo!";
+        return "Hello from Jenkins + Docker + Git Demo!+ WELCOME AZHAR YOU DID GREAT JOB";
     }
 }
